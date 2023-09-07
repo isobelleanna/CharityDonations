3ea81aabcafac31e949d0c4075a3e577d3eef26c
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2023-09-07 20:54:06 +0100

    Task complete

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
 import requests
+import csv
 
 base_url = "https://www.few-far.co/api/techtest/v1"
 
 def get_supporters():
-    supporters_url = f"{base_url}/supporters"
-    response = requests.get(supporters_url)
+    response = requests.get(f"{base_url}/supporters")
     if response.status_code == 200:
         return response.json()["data"]
     else:
@@ -12,8 +12,7 @@ def get_supporters():
         return []
 
 def get_donations():
-    donations_url = f"{base_url}/donations"
-    response = requests.get(donations_url)
+    response = requests.get(f"{base_url}/donations")
     if response.status_code == 200:
         return response.json()["data"]
     else:
@@ -26,3 +25,52 @@ def calculate_supporters_total_value(supporter_id, donations):
         if donation["supporter_id"] == supporter_id:
             total_value += donation["amount"]
     return total_value
+
+
+def export_lifetime_donation_to_csv(supporter_data, output_file):
+    with open(output_file, 'w', newline='') as csvfile:
+        fieldnames = ["Supporter Name", "Total Donation (£)"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for supporter_name, total_value in supporter_data.items():
+            writer.writerow({'Supporter Name': supporter_name, 'Total Donation (£)': f"{total_value:.2f}"})
+
+
+def post_export():
+    response = requests.post(f"{base_url}/donations_exports")
+    if response.status_code == 201:
+        export_data = response.json()
+        export_id = export_data["id"]
+        print("Export successful.")
+        return export_id
+    else:
+        print(f"Status code: {response.status_code}- Export failed")
+        return None
+
+
+def check_export(export_id):
+    response = requests.get(f"{base_url}/donations_exports/{export_id}")
+    if response.status_code == 200:
+        export_data = response.json()
+        if export_data["status"] == "ready":
+            print("Export ready.")
+            print("Export URL:", export_data["url"])
+            return True
+        else:
+            print("Export pending.")
+            return False
+    else:
+        print(f"Status code: {response.status_code}- Checking export status failed")
+        return False
+
+
+def combine_donations(export_url, donations):
+    response = requests.get(export_url)
+    if response.status_code == 200:
+        export_data = response.json()
+        export_donations = export_data.get("data", [])
+        donations.extend(export_donations)
+        print("Export data added to donations.")
+    else:
+        print(f"Status code: {response.status_code}- pulling export data failed")
+
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import time
-from functions import get_supporters, get_donations, calculate_supporters_total_value
+from functions import get_supporters, get_donations, calculate_supporters_total_value, export_lifetime_donation_to_csv, post_export, check_export, combine_donations
+
+base_url = "https://www.few-far.co/api/techtest/v1"
+
 
 def main():
     supporters = get_supporters()
     donations = get_donations()
+
+    export_id = post_export()
+    if export_id:
+        while not check_export(export_id):
+            time.sleep(20)
+        export_url = f"{base_url}/donations_exports/{export_id}"
+        combine_donations(export_url, donations)
 
     if supporters and donations:
         supporter_total = {}
@@ -14,11 +24,11 @@ def main():
             total_value = calculate_supporters_total_value(supporter_id, donations)
             supporter_total[supporter_name] = total_value
 
-        for supporter_name, total_value in supporter_total.items():
-            print(f"{supporter_name}: £{total_value}")
-        else:
-            print("The data requested is unavailable")
+        export_lifetime_donation_to_csv(supporter_total, "lifetime_donation.csv")
+        print("CSV file is ready.")
 
+    else:
+        print("The data requested is unavailable.")
 
 
 if __name__ == "__main__":
